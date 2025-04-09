3707e711e1f49a63445d9f4c7e1966f8ca41ab66
MaNnu81 <mannuspamm@gmail.com>
2025-04-09 10:33:42 +0200

    ok

--- src/app/services/book/book.service.ts
+++ src/app/services/book/book.service.ts
@@ -1,14 +1,16 @@
-import { effect, Injectable, signal } from '@angular/core';
+import { effect, Injectable, InputSignalWithTransform, signal } from '@angular/core';
 import { Book } from '../../model/book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
+ 
 
   readonly BASE_URL = "https://gutendex.com/books/?page="
   page = signal(1)
   books = signal<Book[]>([])
+  selectedBook = signal<Book | undefined>(undefined)
 
   constructor() {
     effect(() => {
@@ -26,4 +28,10 @@ export class BookService {
 
   }
 
+  selectBookById(bookId: number) {
+    const selectBook = this.books().find(book => book.id === bookId)
+    if (selectBook) {
+      this.selectedBook.set(selectBook);
+    }
+  }
 }
